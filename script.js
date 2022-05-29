841022870527094c7f86df7ce0dec3a6ab63734f
GoldGrrl <50558142+carolynraquel@users.noreply.github.com>
2022-05-29 18:50:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//let spam = "Get off my lawn, cowgirl"
+//alert(spam)
+//let num = 44
+
+//if (num == 44){
+  //console.log("yasssss")
+//}else{
+  //console.log("pickle!")
+//}
+
+temperature = -20;
+
+if(temperature > 50){
+  console.log("Mini-skirt pop tart, you better werk")
+  
+} else if (temperature == 50){
+  console.log("Risk it all, homie")
+  
+}else if (temperature > 105){
+  console.log("If you don't keep your crazy butt inside...")
+  
+}else if (temperature < -15){
+  console.log("You lookin like a whole snowglobe")
+  
+}else{
+  console.log("Wear long sleeves, hoochie mama")
+  }
+
+
